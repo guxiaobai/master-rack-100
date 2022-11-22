136200c60de6a50d0ad8dd465eef3030c9d7e07b
guxiaobai <sikuan.gu@gmail.com>
2022-11-22 22:08:20 +0800

    misc

--- potian/ch01/main.rb
+++ potian/ch01/main.rb
@@ -1,5 +1,73 @@
 require 'rack'
+require 'pry'
 
-rack_app = lambda{|env| [200, {}, ["hello from lambda"]]}
-# rack_app = ->(env){}
-Rack::Handler::WEBrick.run rack_app, :Port => 3000
+
+class Proxy
+  attr_reader :env, :manager
+  def initialize(env, manager)
+    @env = env
+    @manager = manager
+  end
+
+
+  def on_request
+    manager._run_callbacks(:on_request, self)
+  end
+
+  def authenticate!
+    throw(:warden)
+  end
+end
+
+class Manager
+  def initialize(app)
+    @app = app
+  end
+
+
+  def call(env)
+    return @app.call(env) if env['warden'] && env['warden'].manager != self
+    env['warden'] = Proxy.new(env, self)
+
+    result || = {}
+
+    case result
+    when Hash 
+       process_unauthenticated(env, result)
+    end
+  end
+
+
+  def process_unauthenticated(env, options={})
+      options[:action] ||= begin
+        opts = config[:scope_defaults][config.default_scope] || {}
+        opts[:action] || 'unauthenticated'
+      end
+
+      proxy  = env['warden']
+      result = options[:result] || proxy.result
+
+      case result
+      when :redirect
+        body = proxy.message || "You are being redirected to #{proxy.headers['Location']}"
+        [proxy.status, proxy.headers, [body]]
+      when :custom
+        proxy.custom_response
+      else
+        options[:message] ||= proxy.message
+        call_failure_app(env, options)
+      end
+    end
+
+end
+
+
+class Application 
+  def call(env)
+    # env['warden'].authenticate!
+    [200, {"Content-Type"=>"text/html"}, ["<h1>Hello, Rack</h1>"]]  
+  end
+end
+
+rack_app = Application.new
+Rack::Handler::WEBrick.run Manager.new(rack_app), :Port => 3000
